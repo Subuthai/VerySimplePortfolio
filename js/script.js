89bcdff4c66d19ed898311fcf0f26f40fecfa278
Subuthai <mryargic@gmail.com>
2021-09-15 11:20:23 +0300

    uploading all files from vsc

---
+++ js/script.js
@@ -0,0 +1,109 @@
+$(document).ready(function () {
+  $("#slides").superslides({
+    animation: "fade",
+    play: 5000,
+    pagination: false,
+  });
+
+  var typed = new Typed(".typed", {
+    strings: ["Jr. Full-Stack", "Video Editor", "Discord Bot Developer"],
+    typeSpeed: 60,
+    startDelay: 900,
+    showCursor: false,
+    loop: true,
+    backDelay: 900,
+    backSpeed: 40,
+  });
+
+  $(".owl-carousel").owlCarousel({
+    loop: true,
+    items: 4,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      480: {
+        items: 2,
+      },
+      768: {
+        items: 3,
+      },
+      938: {
+        items: 4,
+      },
+    },
+  });
+
+  var skillsTopOffset = $(".skillsSection").offset().top;
+  var statsTopOffset = $(".statsSection").offset().top;
+  var countUpFinished = false;
+
+  $(window).scroll(function () {
+    if (window.pageYOffset > skillsTopOffset - $(window).height() + 200) {
+      $(".chart").easyPieChart({
+        easing: "easeInOut",
+        barColor: "#fff",
+        trackColor: false,
+        scaleColor: false,
+        lineWidth: 4,
+        size: 152,
+        onStep: function (from, to, percent) {
+          $(this.el).find(".percent").text(Math.round(percent));
+        },
+      });
+    }
+    if (
+      !countUpFinished &&
+      window.pageYOffset > statsTopOffset - $(window).height() + 200
+    ) {
+      $(".counter").each(function () {
+        var element = $(this);
+        var endVal = parseInt(element.text());
+        element.countup(endVal);
+      });
+      countUpFinished = true;
+    }
+  });
+
+  $("[data-fancybox]").fancybox();
+
+  $("#filters a").click(function () {
+    $("#filters .current").removeClass("current");
+    $(this).addClass("current");
+
+    var selector = $(this).attr("data-filter");
+
+    $(".items").isotope({
+      filter: selector,
+      animationOptions: {
+        duration: 1500,
+        easing: "linear",
+        queue: false,
+      },
+    });
+    return false;
+  });
+
+  $("#navigation li a").click(function (e) {
+    e.preventDefault();
+    var targetElement = $(this).attr("href");
+    var targetPosition = $(targetElement).offset().top;
+    $("html, body").animate({ scrollTop: targetPosition - 50 }, "slow");
+  });
+
+  const nav = $("#navigation");
+  const navTop = nav.offset().top;
+
+  $(window).on("scroll", stickyNavigation);
+
+  function stickyNavigation() {
+    var body = $("body");
+    if ($(window).scrollTop() >= navTop) {
+      body.css("padding-top", nav.outerHeight() + "px");
+      body.addClass("fixedNav");
+    } else {
+      body.css("padding-top", 0);
+      body.removeClass("fixedNav");
+    }
+  }
+});
